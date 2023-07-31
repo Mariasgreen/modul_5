67c3e333ca420a1ff01d4205f02a967659436563
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-07-31 18:55:04 +0300

    Add files via upload

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import opposum from './img/opposum.jpg'
+
+const Module = () => (
+  <>
+    <h1>hello, world</h1>
+    <img src = {opposum}/>
+  </>
+);
+
+export default Module;
